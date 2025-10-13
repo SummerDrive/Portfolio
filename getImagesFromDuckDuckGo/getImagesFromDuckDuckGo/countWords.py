b02012861d90fd2fa8268adb85101c3223787441
SummerDrive <namaikicinq@gmail.com>
2025-10-13 09:23:41 +0900

    Update countWords.py

--- getImagesFromDuckDuckGo/getImagesFromDuckDuckGo/countWords.py
+++ getImagesFromDuckDuckGo/getImagesFromDuckDuckGo/countWords.py
@@ -1,33 +1,58 @@
-﻿import pandas as pd
-import re
 import os
-import time
+import re
+import pandas as pd
+import boto3
+from io import StringIO
+import datetime
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-imageURLs_path = os.path.join(script_dir, 'imageURLs.csv')
-wordCount_path = os.path.join(script_dir, 'wordCount.csv')
+def lambda_handler(event, context):
+    # S3の情報
+    bucket_name = "csvforddg"     # ←バケット名を入れる
+    imageURLs_key = "CSVforDDG/imageURLs.csv"
+    wordCount_key = "CSVforDDG/wordCount.csv"
 
-if __name__ == "__main__":
-    print("countWords")
-    dfW = pd.read_csv(imageURLs_path, header=None)
-    dfC = pd.read_csv(wordCount_path, header=None)
-    dfC.iloc[:, 1] = 0
+    s3 = boto3.client("s3")
 
-    rowCount = dfW.shape[0]
-    for i in range(rowCount):
-        sntnc = dfW.iloc[i,0]
-        wrds = re.findall(r"[A-Za-z]+", sntnc)
-        for wrd in wrds:
-            if wrd in dfC.iloc[:, 0].values:
-                # 存在するなら2列目を+1
-                dfC.loc[dfC.iloc[:, 0] == wrd, 1] += 1
-            else:
-                # 存在しないなら行を追加
-                dfC.loc[len(dfC)] = [wrd, 1]
+    t_start = datetime.datetime.now()
 
-            #print(dfC)
-            #time.sleep(1)
+    try:
+        # --- CSVファイルの読み込み ---
+        objURL = s3.get_object(Bucket=bucket_name, Key=imageURLs_key)
+        dfURL = pd.read_csv(objURL["Body"], header=None, encoding="utf-8-sig", encoding_errors='backslashreplace')
 
-    dfC_sorted = dfC.sort_values(by = 1, ascending = False, kind = "mergesort").reset_index(drop = True)
+        objCount = s3.get_object(Bucket=bucket_name, Key=wordCount_key)
+        dfCount = pd.read_csv(objCount["Body"], header=None, encoding="utf-8-sig", encoding_errors='backslashreplace')
+        dfCount.iloc[:, 1] = 0
 
-    dfC_sorted.to_csv(wordCount_path, index = False, header = False)
+        rowCount = dfURL.shape[0]
+        for i in range(rowCount):
+            sntnc = dfURL.iloc[i,0]
+            wrds = re.findall(r"[A-Za-z]+", sntnc)
+
+            for wrd in wrds:
+                if wrd in dfCount.iloc[:, 0].values:
+                    # 存在するなら2列目を+1
+                    dfCount.loc[dfCount.iloc[:, 0] == wrd, 1] += 1
+                else:
+                    # 存在しないなら行を追加
+                    dfCount.loc[len(dfCount)] = [wrd, 1]
+
+        dfC_sorted = dfCount.sort_values(by = 1, ascending = False, kind = "mergesort").reset_index(drop = True)
+  
+        # --- dfCountを書き戻し ---
+        bufferCount = StringIO()
+        dfCount.to_csv(bufferCount, index=False, header=False)
+        s3.put_object(Bucket=bucket_name, Key=wordCount_key, Body=bufferCount.getvalue())
+
+        t_end = datetime.datetime.now()
+        print("time : ", t_end -t_start)
+            
+        print("countKeyWords実行完了")
+        return {"status": "done"}
+    except Exception as e:
+        t_end = datetime.datetime.now()
+        print("time : ", t_end -t_start)
+
+        print("Error:", e)
+        return {"status": "error", "message": str(e)}
+
