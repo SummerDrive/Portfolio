38033823cc17acc773a8ba090dff7da9b50a3b79
SummerDrive <namaikicinq@gmail.com>
2026-04-05 11:03:26 +0900

    add timer to index2

--- SummerDriveWebMapTest/script_getExpDev(ID秘匿).js
+++ SummerDriveWebMapTest/script_getExpDev(ID秘匿).js
@@ -11,6 +11,7 @@ const baseUrl = "https://API_KEY.execute-api.ap-northeast-1.amazonaws.com";
 
       const row = document.createElement("div");
       row.className = "row";
+      row.style.backgroundColor = "#FFFFFF"
 
       for (let i = 0; i < 5; i++) {
         const span = document.createElement("span");
@@ -22,9 +23,9 @@ const baseUrl = "https://API_KEY.execute-api.ap-northeast-1.amazonaws.com";
       const button = document.createElement("button");
       if (Number(columns[4]) < 12) {
         button.style.backgroundColor = 'red';
-        button.textContent = "�???��??";
+        button.textContent = "少頭数";
       } else {
-        button.textContent = "??????";
+        button.textContent = "反映";
       }
 
       row.appendChild(button);
@@ -43,9 +44,14 @@ const baseUrl = "https://API_KEY.execute-api.ap-northeast-1.amazonaws.com";
       const spanExpDev2 = makeSpan("ExpDev2");
       const spanGetTime = makeSpan("getTime");
 
+      const button2 = document.createElement("button");
+      button2.textContent = "check";
+
+      row.appendChild(button2);
+
       button.onclick = async () => {
-        button.disabled = true; // �?�???��??�???��?��??�?
-        button.innerText = '�?�?';
+        button.disabled = true; // 待機状態（無効化）
+        button.innerText = '待機';
 
         const payload = {
           col1: columns[0],
@@ -70,19 +76,60 @@ const baseUrl = "https://API_KEY.execute-api.ap-northeast-1.amazonaws.com";
           spanExpDev1.textContent = result.data3;
           spanExpDev2.textContent = result.data4;
 
+          const exp1 = Number(spanExpDev1.textContent)
+          const exp2 = Number(spanExpDev2.textContent)
+
+          spanExpDev1.style.backgroundColor = '';
+          spanExpDev2.style.backgroundColor = '';
+
+          if (exp1 > 10000 && exp2 > 10000) {
+            if (exp1 > exp2) {
+              spanExpDev1.style.backgroundColor = 'green';
+            } else if (exp1 < exp2) {
+              spanExpDev2.style.backgroundColor = 'red';
+            }
+          }
+
           spanGetTime.textContent = result.data5;
 
           button.disabled = false;
-          button.innerText = '??????';
+          button.innerText = '反映';
         } catch (error) {
-          alert("???信失???: " + error);
+          alert("送信失敗: " + error);
 
           button.disabled = false;
-          button.innerText = '??????';
+          button.innerText = '反映';
         }
       };
 
-      // row.appendChild(button);
+      button2.onclick = async () => {
+        if (button2.textContent == "check") {
+          button2.textContent = "checked"
+          row.style.backgroundColor = "#2ff016";
+
+          const [hour, minute] = columns[3].split(":").map(Number);
+
+          const now = new Date();
+          const target = new Date();
+
+          target.setHours(hour);
+          target.setMinutes(minute - 10);
+          target.setSeconds(0);
+
+          const diff = target - now;
+
+          alert('タイマーをセットしました。');
+
+          setTimeout(() => {
+            alert("10分前です！");
+            const audio = new Audio("https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg");
+            audio.play();
+          }, diff);
+        } else if (button2.textContent == "checked") {
+          button2.textContent = "check"
+          row.style.backgroundColor = "#FFFFFF";
+        }
+      };
 
       container.appendChild(row);
     });
